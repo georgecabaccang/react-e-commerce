b9de47de668fe10a7486d941766690ae1326aba5
georgesycabaccang <georgesycabaccang@gmail.com>
2024-05-21 14:51:33 +0800

    Intial set up for sign-in feature

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,3 +1,46 @@
+import { useState } from "react";
+
 export default function SignIn() {
-    return <div>Sign In</div>;
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordShown, setPasswordShown] = useState(false);
+
+    function handleEmail(event: React.ChangeEvent<HTMLInputElement>) {
+        setEmail(event.target.value);
+    }
+
+    function handlePassword(event: React.ChangeEvent<HTMLInputElement>) {
+        setPassword(event.target.value);
+    }
+
+    function handleShowPassword() {
+        setPasswordShown((prev) => !prev);
+    }
+
+    return (
+        <div>
+            <label>
+                <div>Email:</div>
+
+                <input value={email} onChange={(event) => handleEmail(event)} name="email" />
+            </label>
+            <label>
+                <div>Password:</div>
+                <input
+                    value={password}
+                    onChange={(event) => handlePassword(event)}
+                    type={passwordShown ? "text" : "password"}
+                    name="password"
+                />
+            </label>
+            <button
+                type="button"
+                onClick={() => {
+                    handleShowPassword();
+                }}
+            >
+                Show Password
+            </button>
+        </div>
+    );
 }
