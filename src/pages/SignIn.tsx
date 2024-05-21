930685393d5ffeb6739a6bb66620387c4167e72a
georgesycabaccang <georgesycabaccang@gmail.com>
2024-05-21 20:29:29 +0800

    Change style of sign in UI.

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -28,26 +28,49 @@ export default function SignIn() {
     }
 
     return (
-        <div className={styles.container}>
-            <h1>Welcome Back!</h1>
-            <Form onSubmit={handleSubmit}>
-                <Form.Input name="email" value={email} onChangeFunction={handleEmail} type="email">
-                    Email:
-                </Form.Input>
-                <Form.Input
-                    name="password"
-                    value={password}
-                    onChangeFunction={handlePassword}
-                    type={passwordShown ? "text" : "password"}
-                    icon={passwordShown ? ShowIcon : HideIcon}
-                    iconFunction={handleShowPassword}
-                >
-                    Password:
-                </Form.Input>
-                <Form.Button type="submit" name="submit">
-                    Submit
-                </Form.Button>
-            </Form>
+        <div className={styles.main_container}>
+            <div className={styles.upper_parent_container}>
+                <div className={styles.container_left}></div>
+                <div className={styles.container_mid}></div>
+                <div className={styles.container_right}></div>
+            </div>
+
+            <div className={styles.parent_container}>
+                <div className={styles.container}></div>
+
+                <div className={styles.main_content}>
+                    <h1>Welcome Back!</h1>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Input
+                            name="email"
+                            value={email}
+                            onChangeFunction={handleEmail}
+                            type="email"
+                        >
+                            Email:
+                        </Form.Input>
+                        <Form.Input
+                            name="password"
+                            value={password}
+                            onChangeFunction={handlePassword}
+                            type={passwordShown ? "text" : "password"}
+                            icon={passwordShown ? ShowIcon : HideIcon}
+                            iconFunction={handleShowPassword}
+                        >
+                            Password:
+                        </Form.Input>
+                        <Form.Button type="submit" name="submit">
+                            Submit
+                        </Form.Button>
+                    </Form>
+                </div>
+                <div className={styles.container}></div>
+            </div>
+            <div className={styles.lower_parent_container}>
+                <div className={styles.container_left}></div>
+                <div className={styles.container_mid}></div>
+                <div className={styles.container_right}></div>
+            </div>
         </div>
     );
 }
