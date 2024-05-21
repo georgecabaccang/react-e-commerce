e4edf41a9be8f4d839eaf324d1e9797e44b9f3ae
georgesycabaccang <georgesycabaccang@gmail.com>
2024-05-21 13:55:11 +0800

    Initial layout for navbar

--- src/components/navigation/navbar/NavBar.tsx
+++ src/components/navigation/navbar/NavBar.tsx
@@ -4,11 +4,11 @@ import styles from "./navbar.module.css";
 
 export default function NavBar() {
     return (
-        <nav>
+        <nav className={styles.nav}>
+            <Link to={PAGES.HOME} className={styles.logo}>
+                Logo For Home
+            </Link>
             <ul className={styles.navbar_list}>
-                <li>
-                    <Link to={PAGES.HOME}>Home</Link>
-                </li>
                 <li>
                     <Link to={PAGES.STORE}>Store</Link>
                 </li>
