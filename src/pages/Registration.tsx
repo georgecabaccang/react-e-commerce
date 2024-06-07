e5cd8859f76e1dcfbb5bd9c753440920841a8f9b
georgesycabaccang <georgesycabaccang@gmail.com>
2024-06-07 11:44:08 +0800

    - Refactor Registration to enable connection to backend for sign-up.

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -9,6 +9,7 @@ import ContentContainer from "../components/reusables/layouts/ContentContainer";
 import { Link } from "react-router-dom";
 import PAGES from "../constants/pages";
 import * as EmailValidator from "email-validator";
+import axios, { AxiosError } from "axios";
 
 export default function Registration() {
     const [email, setEmail] = useState("");
@@ -47,9 +48,22 @@ export default function Registration() {
         setconfirmPasswordShown((prev) => !prev);
     }
 
-    function handleSubmit(event: React.FormEvent) {
-        event?.preventDefault();
-        console.log(email, password);
+    async function handleSubmit(event: React.FormEvent) {
+        event.preventDefault();
+
+        if (!email || !password || !confirmPassword) return console.log("really?");
+
+        try {
+            const { data } = await axios.post("http://localhost:8002/user/sign-up", {
+                email: email,
+                password: password,
+            });
+            console.log(data);
+        } catch (error) {
+            if (error instanceof AxiosError) {
+                console.log(error.response?.data);
+            }
+        }
     }
 
     useEffect(() => {
