9306d453f53aa5e58f4577d95924980e39bc7508
georgesycabaccang <georgesycabaccang@gmail.com>
2024-06-08 16:10:39 +0800

    Use Fake Products API.

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,35 @@
+import { useEffect, useState } from "react";
+import URLS from "../constants/urls";
+import axios from "axios";
+
+interface IProducts {
+    id: number;
+    title: string;
+    price: number;
+    category: string;
+    description: string;
+    image: string;
+    rating: {
+        rate: number;
+        count: number;
+    };
+}
+
 export default function Store() {
+    const [products, setProducts] = useState<IProducts[] | null>(null);
+
+    async function getProducts() {
+        const { data } = await axios.get(URLS.STORE_PRODUCTS);
+        return setProducts(data);
+    }
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
+    useEffect(() => {
+        console.log(products);
+    }, [products]);
+
     return <div>Store</div>;
 }
