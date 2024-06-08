306c8e711e1ac7c5bcc78f171e66bcd4106e718f
georgesycabaccang <georgesycabaccang@gmail.com>
2024-06-08 13:38:23 +0800

    Add URL constants.

---
+++ src/constants/urls.ts
@@ -0,0 +1,7 @@
+enum URLS {
+    SERVER_BASE = "http://locahost:8002",
+    SEVER_USER_SIGN_IN = `${SERVER_BASE}/sign-in`,
+    SEVER_USER_SIGN_UP = `${SERVER_BASE}/sign-up`,
+}
+
+export default URLS;
