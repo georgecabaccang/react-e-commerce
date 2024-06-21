016734040cf446c6af7d82e89827084a0ae1f08f
georgesycabaccang <georgesycabaccang@gmail.com>
2024-06-21 19:41:41 +0800

    Create cartSlice for cart store feature.

---
+++ src/store/cartStore/cartSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface IItem {
+    id: number;
+    title: string;
+    price: number;
+    quantity: number;
+}
+
+interface ICart {
+    _id?: string;
+    items: IItem[] | [];
+}
+
+const initialState: ICart = {
+    _id: "",
+    items: [],
+};
+
+export const cartSlice = createSlice({
+    name: "cart",
+    initialState,
+    reducers: {
+        loadCart: (state, action: PayloadAction<ICart>) => {
+            state._id = action.payload._id;
+            state.items = action.payload.items;
+        },
+    },
+});
+
+export const { loadCart } = cartSlice.actions;
+export default cartSlice.reducer;
