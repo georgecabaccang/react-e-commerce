12df0ddc57160e6d7ffc626ce668f155c6953b39
George <91815033+georgecabaccang@users.noreply.github.com>
2024-06-27 14:02:22 +0800

    Refactor Quantity of cart compount component and QuantityBox.tsx (#27)
    - Change how changeDBQuantity is handled.
    - Refactor changeDBQuantity into a useCallback function to stop requesting to backend server when quantity limits are reached.

--- src/components/cart/compound-components/Quantity.tsx
+++ src/components/cart/compound-components/Quantity.tsx
@@ -2,6 +2,7 @@ import styles from "../CartItemList.module.css";
 import QuantityBox from "../../reusables/inputs/quantity-box/QuantityBox";
 import { IItem } from "../../../store/cartStore/cartSlice";
 import useChangeQuantity from "../../../hooks/wrapers/cart/useChangeQuantity";
+import { useCallback } from "react";
 
 export default function Quantity({
     quantity,
@@ -16,16 +17,29 @@ export default function Quantity({
 }) {
     const { newQuantity, changeQuantity } = useChangeQuantity();
 
-    async function changeDBQuantity(quantity: number) {
-        console.log(newQuantity);
-        // if (newQuantity < lowerLimit || newQuantity > higherLimit) return;
-        await changeQuantity({
-            id: item.id,
-            title: item.title,
-            price: item.price,
-            quantity: quantity,
-        });
-    }
+    const changeDBQuantity = useCallback(
+        async (quantity: number) => {
+            if (quantity < lowerLimit || quantity > higherLimit) return;
+            await changeQuantity({
+                id: item.id,
+                title: item.title,
+                price: item.price,
+                quantity: quantity,
+            });
+        },
+        [changeQuantity, item, lowerLimit, higherLimit]
+    );
+
+    // async function changeDBQuantity(quantity: number) {
+    //     console.log(newQuantity);
+    //     if (newQuantity < lowerLimit || newQuantity > higherLimit) return;
+    //     await changeQuantity({
+    //         id: item.id,
+    //         title: item.title,
+    //         price: item.price,
+    //         quantity: quantity,
+    //     });
+    // }
 
     return (
         <div className={styles.cart_item__quantity}>
